029a7314a9f0f7d73ff855911520731645ca204b
Bradley Calkins <b.raders1013@gmail.com>
2020-06-11 00:50:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,19 +5,28 @@ p  num1+num2
 end
 
 def subtraction(num1, num2)
+  num1 = 10 
+  num2 = 5
+p num1 - num2
 
 end
 
 def division(num1, num2)
-
+  num1 = 50
+  num2 = 2
+p  num1/num2
+  
 end
 
 def multiplication(num1, num2)
+  num1 = 4
+  num2 = 30
+p num1*num2 
 
 end
 
 def modulo(num1, num2)
-
+  num1 = 
 end
 
 def square_root(num)
